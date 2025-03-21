63936c8df4d83d83e0104cb5b3e8f2bee9578dfa
Bstteam108 <bstteam108@gmail.com>
2025-03-21 15:37:26 +0430

    large file

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 const app = express();
 
 // ✅ Place these first
-app.use(express.json()); // Parses JSON requests
-app.use(express.urlencoded({ extended: true })); // Parses form data
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Security & other middleware
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 
-app.use(express.json({ limit: "50mb" })); // JSON payload limit increase
-app.use(express.urlencoded({ extended: true, limit: "50mb" })); // URL-encoded payload limit
+
 
 // MongoDB Connection
 
