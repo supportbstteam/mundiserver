926b863a72d4748340257995ccbf32c13a7f1254
Bstteam108 <bstteam108@gmail.com>
2025-03-21 15:07:42 +0430

    large file

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -54,7 +54,7 @@ export const excelDataStore = async (req, res) => {
 
         const result = new ExcelData({
             userId: req.body.userId,
-            data: JSON.parse(req.body.data.toString()),
+            data: req.body.data,
         });
 
         const dt = await result.save();
